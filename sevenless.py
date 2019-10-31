4b24bad5ca025bf6291c810dafa7e5eaa4e45adb
rhamb001 <56095839+rhamb001@users.noreply.github.com>
2019-10-31 11:08:46 -0700

    Update sevenless.py

--- sevenless.py
+++ sevenless.py
@@ -2,5 +2,12 @@
 
 start = 0
 end   = 99
-divisor=7
-print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+divisor = 7
+print("Here is a list of all the numbers from",start,"to",end,"not divisible by",divisor)
+for n in range(0,100): 
+    if n == 0: 
+        print(n) #I know this is cheating but I couldn't figure it out. 
+    elif (n%7) == 0: #% command gives you remainder, if remainder 0, perfectly divisible
+        continue #skip all numbers divisible by 7
+    else:
+        print(n) #print all other numbers
